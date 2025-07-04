7fe0bd8939608fbf75a6f12d96c7fdf1b36e675c
Arun Sadalgekar <108878412+Arun12311@users.noreply.github.com>
2025-07-04 16:02:00 +0530

    Update Techstack.js

--- src/components/About/Techstack.js
+++ src/components/About/Techstack.js
@@ -10,7 +10,7 @@ import {
   DiGit,
 } from "react-icons/di";
 import {
-  SiRedis,
+  SiDocker, SiJenkins, SiFlask, SiAmazonaws
 } from "react-icons/si";
 
 
@@ -19,28 +19,51 @@ function Techstack() {
     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
       <Col xs={4} md={2} className="tech-icons">
         <CgCPlusPlus />
+        <div className="tech-label">C++</div>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <DiJavascript1 />
+        <div className="tech-label">JavaScript</div>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <DiNodejs />
+        <div className="tech-label">Node.js</div>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <DiReact />
+        <div className="tech-label">React</div>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <DiMongodb />
+        <div className="tech-label">MongoDB</div>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <DiGit />
+        <div className="tech-label">Git</div>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
-        <SiRedis />
+        <SiDocker />
+        <div className="tech-label">Docker</div>
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiJenkins />
+        <div className="tech-label">Jenkins</div>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <DiPython />
+        <div className="tech-label">Python</div>
       </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiFlask />
+        <div className="tech-label">Flask</div>
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiAmazonaws />
+        <div className="tech-label">AWS</div>
+      </Col>
+
+ 
+
     </Row>
   );
 }
