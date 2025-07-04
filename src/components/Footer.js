c2d54b4b6fefa0c5dc00fc3890b7030b640f1022
Arun Sadalgekar <108878412+Arun12311@users.noreply.github.com>
2025-07-04 16:04:14 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -13,7 +13,7 @@ function Footer() {
     <Container fluid className="footer">
       <Row>
         <Col md="4" className="footer-copywright">
-          <h3>Designed and Developed by Arun Sadalgekar</h3>
+          <h3>Developed by Arun Sadalgekar</h3>
         </Col>
         <Col md="4" className="footer-copywright">
           <h3>Copyright © {year} AS</h3>
@@ -22,7 +22,7 @@ function Footer() {
           <ul className="footer-icons">
             <li className="social-icons">
               <a
-                href="https://github.com/Arun12311"
+                href="https://github.com/ArunSadalgekar07"
                 style={{ color: "white" }}
                 target="_blank" 
                 rel="noopener noreferrer"
